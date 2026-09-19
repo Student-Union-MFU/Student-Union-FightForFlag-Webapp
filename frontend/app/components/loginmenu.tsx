00e2f7bffdb3c373ec690b89b06273a898591342
Yion <6831503029@lamduan.mfu.ac.th>
2026-09-19 11:23:37 +0700

    Login fix

--- frontend/app/components/loginmenu.tsx
+++ frontend/app/components/loginmenu.tsx
@@ -2,9 +2,11 @@
 
 import clsx from "clsx";
 
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export default function LoginMenu() {
     const startGoogleLogin = () => {
-        window.location.href = "http://localhost:8000/auth/login";
+        window.location.href = `http://${backendUrl}/auth/login`;
     };
 
     const handleLoginOnClick = () => {
