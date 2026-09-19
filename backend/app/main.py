0c44e146428e155276de0d8f7798b8700ff2fd68
Yion <6831503029@lamduan.mfu.ac.th>
2026-09-19 10:29:45 +0700

    Backend Expose

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,3 +37,7 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(vote.router)
+
+@app.get("/")
+def root():
+    return {"message": "Fight For Flag API"}
