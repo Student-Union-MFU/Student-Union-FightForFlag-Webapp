de91f24c8ab11184b94bebe98a85f819e514e3bf
Yion <6831503029@lamduan.mfu.ac.th>
2026-09-20 10:53:00 +0700

    ...

--- backend/app/auth/oauth.py
+++ backend/app/auth/oauth.py
@@ -9,3 +9,4 @@ oauth.register(
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
     client_kwargs={"scope": "openid email profile"},
 )
+print("GOOGLE CLIENT ID:", settings.google_client_id)
